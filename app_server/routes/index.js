1097c16f022e49192e101153432145ecc9250845
RichardSteel <mrrichardsteel@gmail.com>
2016-08-23 21:26:42 -0400

    Add files via upload

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,11 +4,17 @@ var ctrlArcher = require('../controllers/archer');
 var ctrlCoach = require('../controllers/coach');
 var ctrlHome = require('../controllers/home');
 var ctrlInfo = require('../controllers/info');
+var ctrlSignUp = require('../controllers/signup');
+var ctrlEquipment = require('../controllers/equipment');
+var ctrlCoachEquip = require('../controllers/coachEquip');
 
 /* GET home page. */
 router.get('/archer', ctrlArcher.studentInfo);
 router.get('/coach', ctrlCoach.coachLogin);
 router.get('/', ctrlHome.landing);
 router.get('/info', ctrlInfo.archeryInfo);
+router.post('/signin', ctrlSignUp.coachArea);
+router.get('/equipment', ctrlEquipment.equipmentList);
+router.get('/coachEquip', ctrlCoachEquip.coachEquip);
 
 module.exports = router;
